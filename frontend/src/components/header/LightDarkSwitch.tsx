e8d0a9b823f2340a6961f16f88e40664c0d32c84
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-17 23:46:51 +0100

    Add functionality to the LightDarkSwitch component to enable switching between the light and dark theme

--- frontend/src/components/header/LightDarkSwitch.tsx
+++ frontend/src/components/header/LightDarkSwitch.tsx
@@ -1,13 +1,31 @@
 import SunIcon from "../icons/SunIcon.tsx";
 import MoonIcon from "../icons/MoonIcon.tsx";
 import "./LightDarkSwitch.css";
+import {useEffect, useState} from "react";
 
 function LightDarkSwitch() {
+    const [isChecked,setIsChecked] = useState(true);
+
+    useEffect(() => {
+        if(isChecked) {
+            document.documentElement.classList.remove("light");
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            document.documentElement.classList.add("light");
+        }
+    }, [isChecked])
+
+    function handleClick() {
+        setIsChecked(!isChecked);
+    }
+
+
     return (
         <div className={"toggle-switch-container"}>
             <SunIcon />
             <label className={"switch"}>
-                <input type={"checkbox"}/>
+                <input type={"checkbox"} checked={isChecked} onClick={handleClick}/>
                 <span className={"slider"}></span>
             </label>
             <MoonIcon />
