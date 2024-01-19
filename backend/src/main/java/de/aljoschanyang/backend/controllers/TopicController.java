a9c6df7b459455b0ae1de6f176cdf0820bf105fb
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-20 00:00:31 +0100

    Add get mapping in the controller to retrieve all topics

--- backend/src/main/java/de/aljoschanyang/backend/controllers/TopicController.java
+++ backend/src/main/java/de/aljoschanyang/backend/controllers/TopicController.java
@@ -1,13 +1,21 @@
 package de.aljoschanyang.backend.controllers;
 
+import de.aljoschanyang.backend.models.Topic;
+import de.aljoschanyang.backend.models.TopicDTO;
 import de.aljoschanyang.backend.services.TopicService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/topics")
 public class TopicController {
     private final TopicService topicService;
+
+    @GetMapping
+    public List<TopicDTO> getAllTopics() {
+        return topicService.getAllTopics();
+    }
 }
