a1a456b784cef346293e15c945554f21c75db8c8
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-19 23:59:45 +0100

    Create helper method to convert the entity to a DTO and create getAllTopics method in the service layer

--- backend/src/main/java/de/aljoschanyang/backend/services/TopicService.java
+++ backend/src/main/java/de/aljoschanyang/backend/services/TopicService.java
@@ -1,12 +1,33 @@
 package de.aljoschanyang.backend.services;
 
+import de.aljoschanyang.backend.models.Topic;
+import de.aljoschanyang.backend.models.TopicDTO;
 import de.aljoschanyang.backend.repositories.TopicRepo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class TopicService {
     private final TopicRepo topicRepo;
 
+    private TopicDTO convertToDTO(Topic topic) {
+        return TopicDTO.builder()
+                .id(topic.id())
+                .title(topic.title())
+                .icon(topic.icon())
+                .category(topic.category())
+                .build();
+    }
+
+    public List<TopicDTO> getAllTopics() {
+        List<Topic> topics = topicRepo.findAll();
+
+        return topics.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 }
