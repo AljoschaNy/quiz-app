43ca5a36082a5b3a6417407efecab6c8d9a68774
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-22 16:10:32 +0100

    Override equals, hashCode, and toString in Question and QuestionDTO for array content comparison

--- backend/src/main/java/de/aljoschanyang/backend/models/Question.java
+++ backend/src/main/java/de/aljoschanyang/backend/models/Question.java
@@ -5,6 +5,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 @Builder
 @Document(collection = "questions")
 public record Question(
@@ -17,4 +20,35 @@ public record Question(
         String[] options,
         String answer
 ) {
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Question question = (Question) o;
+                return difficulty == question.difficulty &&
+                        Objects.equals(id, question.id) &&
+                        Objects.equals(topicId, question.topicId) &&
+                        Objects.equals(query, question.query) &&
+                        Arrays.equals(options, question.options) &&
+                        Objects.equals(answer, question.answer);
+        }
+
+        @Override
+        public int hashCode() {
+                int result = Objects.hash(id, topicId, difficulty, query, answer);
+                result = 31 * result + Arrays.hashCode(options);
+                return result;
+        }
+
+        @Override
+        public String toString() {
+                return "Question{" +
+                        "id='" + id + '\'' +
+                        ", topicId='" + topicId + '\'' +
+                        ", difficulty=" + difficulty +
+                        ", query='" + query + '\'' +
+                        ", options=" + Arrays.toString(options) +
+                        ", answer='" + answer + '\'' +
+                        '}';
+        }
 }
--- backend/src/main/java/de/aljoschanyang/backend/models/QuestionDTO.java
+++ backend/src/main/java/de/aljoschanyang/backend/models/QuestionDTO.java
@@ -2,6 +2,9 @@ package de.aljoschanyang.backend.models;
 
 import lombok.Builder;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 @Builder
 public record QuestionDTO(
         String id,
@@ -9,4 +12,31 @@ public record QuestionDTO(
         String[] options,
         String answer
 ) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuestionDTO that = (QuestionDTO) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(query, that.query) &&
+                Arrays.equals(options, that.options) &&
+                Objects.equals(answer, that.answer);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, query, answer);
+        result = 31 * result + Arrays.hashCode(options);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionDTO{" +
+                "id='" + id + '\'' +
+                ", query='" + query + '\'' +
+                ", options=" + Arrays.toString(options) +
+                ", answer='" + answer + '\'' +
+                '}';
+    }
 }
