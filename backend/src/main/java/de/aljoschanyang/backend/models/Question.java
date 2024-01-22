68d696d892a79bbf198456b7efe1f7ca151130c7
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-22 12:59:02 +0100

    Add reference to Topic entity

--- backend/src/main/java/de/aljoschanyang/backend/models/Question.java
+++ backend/src/main/java/de/aljoschanyang/backend/models/Question.java
@@ -3,12 +3,15 @@ package de.aljoschanyang.backend.models;
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Builder
 @Document(collection = "questions")
 public record Question(
         @Id
         String id,
+        @Field("topic_id")
+        String topicId,
         int difficulty,
         String query,
         String[] options,
