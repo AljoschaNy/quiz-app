13d3a5c2dcf94ddbe6d1ba9d8fb8d51f34961ed5
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-22 09:50:05 +0100

    add get request to the StartPage to use real data from the db and refactor MainContent and OptionCard components

--- frontend/src/components/main/MainContent.tsx
+++ frontend/src/components/main/MainContent.tsx
@@ -1,15 +1,8 @@
 import "./MainContent.css";
 import OptionCard from "./OptionCard.tsx";
-import {Topic} from "../../types/types.ts";
+import {MainContentType} from "../../types/types.ts";
 
-function MainContent() {
-
-    const topicsArray:Topic[] = [
-        {id: "1", name:"HTML",path:"assets/icon-html.svg"},
-        {id: "2", name:"CSS", path:"assets/icon-css.svg"},
-        {id: "3", name:"Javascript", path:"assets/icon-js.svg"},
-        {id: "4", name:"Accessibility", path:"assets/icon-accessibility.svg"}
-    ];
+function MainContent( {topics}:Readonly<MainContentType> ) {
 
     return (
         <main className={"main-content"}>
@@ -19,7 +12,7 @@ function MainContent() {
             </section>
 
             <section className={"options-container"}>
-                {topicsArray.map(topic => {
+                {topics.map(topic => {
                     return <OptionCard key={topic.id} topic={topic}/>;
                 })}
             </section>
--- frontend/src/components/pages/StartPage.tsx
+++ frontend/src/components/pages/StartPage.tsx
@@ -1,8 +1,19 @@
 import MainContent from "../main/MainContent.tsx";
+import {Topic} from "../../types/types.ts";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function StartPage() {
+    const [topics, setTopics] = useState<Topic[]>([])
+
+    useEffect(() => {
+        axios.get("/api/topics")
+            .then(response => setTopics(response.data))
+            .catch(error => console.log("error", error))
+    })
+
     return (
-        <MainContent />
+        <MainContent topics={topics} />
     );
 }
 
