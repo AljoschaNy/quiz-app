fe951b89b8ae5bc049bbf4d4b0dc731af7eda81d
Aljoscha Nyang <a.nyang@gmx.de>
2024-01-29 19:44:35 +0100

    Show all questions one after the other upon submitting an answer

--- frontend/src/components/pages/QuestionPage.tsx
+++ frontend/src/components/pages/QuestionPage.tsx
@@ -6,8 +6,9 @@ import OptionCard from "../main/OptionCard.tsx";
 
 function QuestionPage() {
     const {topicId} = useParams();
-    const [questions, setQuestions] = useState<Question[]>()
+    const [questions, setQuestions] = useState<Question[]>([])
     const [selectedOption, setSelectedOption] = useState<string | null>(null);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
     const quizAnswers:string[] = ["A","B","C","D"];
 
     useEffect(() => {
@@ -23,22 +24,23 @@ function QuestionPage() {
     }
 
     function handleSubmit() {
+        currentQuestion < questions.length-1 ? setCurrentQuestion(currentQuestion+1) : alert("finish");
         setSelectedOption(null)
     }
 
-    return questions && (
+    return questions.length > 0 && (
         <main className={"main-content"}>
             <section className={"info-section"}>
                 <p className={"info-text"}>Question 6 of 10</p>
-                <h2>{questions[0].query}</h2>
+                <h2>{questions[currentQuestion].query}</h2>
                 <div className={"progress-bar"}></div>
             </section>
 
             <section className={"options-container"}>
                 {
-                    questions[0].options.map((option, index) => {
+                    questions[currentQuestion].options.map((option, index) => {
                     return <OptionCard
-                        key={questions[0]+option}
+                        key={questions[currentQuestion]+option}
                         quizAnswer={quizAnswers[index]}
                         optionText={option}
                         isSelected={selectedOption === option}
